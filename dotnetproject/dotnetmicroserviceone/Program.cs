0e271a993d9d9e7e4bf6ccb9918183063332f202
Iamneo <techadmin@iamneo.ai>
2023-10-05 09:21:39 +0000

    Backup Commit

--- dotnetproject/dotnetmicroserviceone/Program.cs
+++ dotnetproject/dotnetmicroserviceone/Program.cs
@@ -6,7 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.addDbContext<CustomerDbContext>(options =>{
+    options.UseSqlServer(builder.Configuration.GetConnectionstring)
+})
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
