9ae14cd9fbded2efe8de5f8f23f75a5b8a1181ee
Iamneo <techadmin@iamneo.ai>
2023-10-05 08:41:39 +0000

    Backup Commit

--- dotnetproject/dotnetmicroserviceone/Models/Customer.cs
+++ dotnetproject/dotnetmicroserviceone/Models/Customer.cs
@@ -7,6 +7,9 @@ namespace dotnetmicroserviceone.Models
 {
     public class Customer
     {
-        
-    }
+        public int CustomerId{get; set;} 
+        public string CustomerName{get; set;}
+        public string MobileNumber{get; set;}
+        public string Email{get; set;}
+   }
 }
