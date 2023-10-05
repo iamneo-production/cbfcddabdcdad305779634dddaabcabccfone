9795a22049907946bd67885c3bc2ddf3dbc10e00
Iamneo <techadmin@iamneo.ai>
2023-10-05 08:51:39 +0000

    Backup Commit

--- dotnetproject/dotnetmicroserviceone/Models/CustomerDbContext.cs
+++ dotnetproject/dotnetmicroserviceone/Models/CustomerDbContext.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnetmicroserviceone.Models
 {
     public class CustomerDbContext
     {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext>options):base(options){
+
+        }
         
     }
 }
